817809522830ff95a3b2f92bfbbc073cf511ef3b
Hilbert Muniz de Andrade <hilbertmunizdeandrade@gmail.com>
2023-06-15 21:34:14 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,83 @@
+// // Isso é um comentário  
 
+// /*Eu quero fazer 
+// comentário que está
+// em varias linhas
+// por que eu 
+// sou MutationObserver
+// poeta
+// */
+
+// //console.log("Olá, mundo!")
+// //console.log("Oi cú cagado,kk")
+
+
+// //prompt("Como vc se chama?")
+// //prompt("Qual é sua idade?")
+
+// const nome = "Hilbert Muniz de Andrade";
+// console.log(nome)
+
+// // nome = "Hilbert Muniz"
+// // console.log(idade)
+
+// let idade = 17
+// console.log(idade)
+
+// idade = 41
+// console.log("Olá", "Meu nome é", nome, "e eu tenho", idade, "Anos")
+// let temPet = false
+
+// if (temPet === true){
+//     console.log("Tenho pet")
+// }else{
+//     console.log("Não tenho pet")
+// }
+
+// const nomePet = null
+
+// const nome = prompt('Qual é o seu nome?')
+
+// console.log(nome)
+// console.log(typeof nome)
+
+// let idade = Number(prompt('Qual é a sua idade?') )
+// console.log(idade)
+// console.log(typeof idade)
+
+// const numero = '18'
+// console.log(typeof numero)
+
+// const primeiroNumero = (prompt("Fale um número"))
+// const segundoNumero = (prompt("Digite um número"))
+
+// const resultado = primeiroNumero + segundoNumero
+// console.log("resultado")
+
+// const nomeDoObjeto = "caixinha de som"
+// let quantidadeDeBotoes = 5
+// let estaLigada = false
+
+// console.log("Tipo da variavel nomeDoObjeto", typeofnomeDoObjeto)
+ 
+// console.log("Tipo da Vriavel quantidadeDeBotoes", typeof quantidadeDeBotoes)
+
+// console.log("Tipo da variavel estaLigada", typeof estaLigada)
+
+// console.log("Eu escolhi o Objeto", nomeDoObjeto, "que tem", quantidadeDeBotoes, "botoes", "O objeto está Ligado?", estaLigada)
+
+
+let valorDeA = 30
+let valorDeB =20
+let valorDC 
+
+c=a 
+console.log("C:", c)
+
+a=b
+
+console.log("A:", a)
+
+b=c
+
+console.log("B", b)
